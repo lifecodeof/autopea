8d4b33dd196eab77a641dc1d891f79f1a40fe7c4
Semih Koyuncu <42251538+lifecodeof@users.noreply.github.com>
2025-07-28 05:17:18 +0300

    use .ts files instead of .d.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   // entry: ["src", "!**/*.snap", "!**/*.test.ts"],
   entry: ["src/index.ts", "src/ffi/*.ts", "!**/*.test.ts"],
+  
+  dts: false,
 
   sourcemap: true,
   format: "esm",
   target: "node24",
-  bundle: true,
-  dts: true,
   clean: true,
+  bundle: false,
   splitting: false
 })
