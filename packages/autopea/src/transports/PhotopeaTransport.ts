fcdd752723eb868c5127eb35ea88ea84b2a119a2
Semih Koyuncu <42251538+lifecodeof@users.noreply.github.com>
2026-02-12 00:55:56 +0300

    format code

--- packages/autopea/src/transports/PhotopeaTransport.ts
+++ packages/autopea/src/transports/PhotopeaTransport.ts
@@ -4,7 +4,7 @@ export type PhotopeaTransportEventMap = {
   /**
    * Emitted when a blank message is received from Photopea. Photopea indicates
    * things like document load completion by sending an empty message.
-   * 
+   *
    * Transport implementors should buffer messages between "done" messages to
    * determine whether the message is really blank or just a part of a larger message.
    */
@@ -14,12 +14,11 @@ export type PhotopeaTransportEventMap = {
    * Some transports may not support this event. */
   pageerror: (error: Error) => void
 
-  /** Emitted when `_pp_sendResponse` callback is called from Photopea realm. */ 
+  /** Emitted when `_pp_sendResponse` callback is called from Photopea realm. */
   response: (id: string, reqType: string, data: unknown) => void
 }
 
 export interface PhotopeaTransport {
-
   /** Registers an event handler for a specific Photopea transport event.
    * @param event The name of the event to listen for.
    * @param handler The callback function to invoke when the event occurs.
@@ -40,7 +39,7 @@ export interface PhotopeaTransport {
 
   /**
    * Provides access to the capabilities of the Photopea transport.
-   * Some contract methods will use this object if there is no ActionScript 
+   * Some contract methods will use this object if there is no ActionScript
    * way to achieve the desired functionality.
    */
   get capabilities(): PhotopeaCapabilities
